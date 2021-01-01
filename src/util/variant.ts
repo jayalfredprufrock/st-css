ad6da80d48b39f3df71ae655385f4db4af13f4b6
Andrew Smiley <jayalfredprufrock@gmail.com>
2020-12-31 16:34:21 -0800

    Allow variants to specify styles as a function.

--- src/util/variant.ts
+++ src/util/variant.ts
@@ -1,6 +1,7 @@
 import type { StCss } from "../st-css";
 
-export const variant = <V extends string>(vProp: string, vStyles: Record<V, Styles>, vDefault?: V) => (props: any, stCss: StCss) => {
-    const vVal = props[vProp] || vDefault;
-    return Array.isArray(vVal) ? vVal.map(v => stCss.extractRulesByBp(vStyles[v as V])[0]) : vStyles[vVal as V];
+export const variant = <V extends string>(vProp: string, vStyles: Record<V, Styles> | ((props: any) => Record<V, Styles>), vDefault?: V) => (props: any, stCss: StCss) => {
+    const vVal: V | V[] = props[vProp] || vDefault;
+    const styles = typeof vStyles === 'function' ? vStyles(props) : vStyles;
+    return Array.isArray(vVal) ? vVal.map(v => stCss.extractRulesByBp(styles[v])[0]) : styles[vVal];
 }
