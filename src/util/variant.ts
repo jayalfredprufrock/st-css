64575b2974e201534bb7dd1d65101f3d693451c3
Andrew Smiley <jayalfredprufrock@gmail.com>
2020-12-31 17:46:25 -0800

    Fix bug with responsive variants.

--- src/util/variant.ts
+++ src/util/variant.ts
@@ -3,5 +3,8 @@ import type { StCss } from "../st-css";
 export const variant = <V extends string>(vProp: string, vStyles: Record<V, Styles> | ((props: any) => Record<V, Styles>), vDefault?: V) => (props: any, stCss: StCss) => {
     const vVal: V | V[] = props[vProp] || vDefault;
     const styles = typeof vStyles === 'function' ? vStyles(props) : vStyles;
-    return Array.isArray(vVal) ? vVal.map(v => stCss.extractRulesByBp(styles[v])) : styles[vVal];
+    return Array.isArray(vVal) ? vVal.map((v, i) => {
+        const rules = stCss.extractRulesByBp(styles[v]);
+        return Object.assign(rules[0], rules[i]);
+    }) : styles[vVal];
 }
