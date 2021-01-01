1b19b51cecd360a3df01495eeec8950ca86bf746
Andrew Smiley <jayalfredprufrock@gmail.com>
2020-12-31 16:43:26 -0800

    Allow variants to contain responsive styles.

--- src/util/variant.ts
+++ src/util/variant.ts
@@ -3,5 +3,5 @@ import type { StCss } from "../st-css";
 export const variant = <V extends string>(vProp: string, vStyles: Record<V, Styles> | ((props: any) => Record<V, Styles>), vDefault?: V) => (props: any, stCss: StCss) => {
     const vVal: V | V[] = props[vProp] || vDefault;
     const styles = typeof vStyles === 'function' ? vStyles(props) : vStyles;
-    return Array.isArray(vVal) ? vVal.map(v => stCss.extractRulesByBp(styles[v])[0]) : styles[vVal];
+    return Array.isArray(vVal) ? vVal.map(v => stCss.extractRulesByBp(styles[v])) : styles[vVal];
 }
