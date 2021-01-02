f67c0464e97aeedf61dacc50e46d2e0619142f1b
Andrew Smiley <jayalfredprufrock@gmail.com>
2021-01-01 16:29:15 -0800

    Allow styleProps util to accept arrays of props.

--- src/util/styleProps.ts
+++ src/util/styleProps.ts
@@ -1,5 +1,5 @@
 import type { StCss } from "../st-css";
 
-export const styleProps = (...styleProps: string[]) => (props: any, _: StCss): Styles => {
-    return styleProps.reduce((acc, p) => ({ ...acc, [p]: props[p] }), {} as Styles);
+export const styleProps = (...styleProps: (string | string[])[]) => (props: any, _: StCss): Styles => {
+    return ([] as string[]).concat(...styleProps).reduce((acc, p) => ({ ...acc, [p]: props[p] }), {} as Styles);
 }
